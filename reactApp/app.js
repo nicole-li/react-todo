fcd8365ac1a3e3a2935d9beed2ba67902dcb86e8
Nicole Li <li.nicole160@gmail.com>
2018-06-29 16:59:27 -0700

    bulleted list of static data

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Something Else</p>,
+var dummyData = ['Come to SF', 'Master React', 'Sleep'];
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {dummyData.map((todo) => <Todo task={todo}/>)}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <li><button>X</button>{this.props.task}</li>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
