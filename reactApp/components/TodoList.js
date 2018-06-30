055b02fddd50eac12af36c93f9e32bca6122c5b3
Nicole Li <li.nicole160@gmail.com>
2018-06-29 18:22:28 -0700

    handle typing implemented

--- reactApp/components/TodoList.js
+++ reactApp/components/TodoList.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Todo from './Todo.js';
 
 class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
   render() {
     return (
       <ul>
-        {this.props.todos.map((todo) => <Todo task={todo.taskText} completed={todo.completed}/>)}
+        {this.props.todos.map((todo) =>
+          <Todo task={todo.taskText} completed={todo.completed}/>
+        )}
       </ul>
     )
   }
